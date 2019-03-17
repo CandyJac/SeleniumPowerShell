6d4db560b6b577c34420dae7db9907dbe2feea4a
Jacques van Rensburg <jvanrensburg@live.co.za>
2019-03-17 14:33:31 +0200

    Add files via upload

---
+++ Start-SeleniumBrowser.ps1
@@ -0,0 +1,31 @@
+﻿ Function Start-SeleniumBrowser {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Chrome','IE','Firefox')]
+        [String]
+        $Browser,
+        
+        [Parameter(Mandatory = $false)]
+        $Url
+        )
+           
+        switch ($Browser) {
+            'Chrome' {
+               $global:WebDriver = New-Object -TypeName OpenQA.Selenium.Chrome.ChromeDriver
+                  }
+            
+            'IE' {
+               $global:WebDriver = New-Object -TypeName OpenQA.Selenium.IE.InternetExplorerDriver
+                  }
+            
+            'Firefox' {
+               $global:WebDriver = New-Object -TypeName OpenQA.Selenium.Firefox.FirefoxDriver
+                }
+         }
+
+            $global:WebDriver.Navigate().GoToUrl("http://www." + $Url)
+
+}
+
+
